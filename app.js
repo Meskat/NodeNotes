5d554d9eda866c933ef39f840215fb3f2d2fe272
Meskat <marciniszyn.k@gmail.com>
2018-06-25 22:04:09 +0200

    Remove age from greeting

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ const onError = (err) => {
   if (!err) return;
   console.log('Sorry, we were not able to create that note! See why -->', err);
 };
-fs.appendFile('greet-user.txt', `Hello ${user.username}, you are ${notes.age} \n `, onError);
+fs.appendFile('greet-user.txt', `Hello ${user.username} \n `, onError);
