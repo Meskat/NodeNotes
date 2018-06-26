bb356286a97c1052c06c8ffc0bb09d18cf20a3de
Meskat <marciniszyn.k@gmail.com>
2018-06-26 21:31:56 +0200

    Add yargs to project,create switch statement to react on commants, create matching functions

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 const notes = require('./notes.js');
+const argv = yargs.argv;
+const command = argv._[0];
 
-let user = os.userInfo();
 
-const onError = (err) => {
-  if (!err) return;
-  console.log('Sorry, we were not able to create that note! See why -->', err);
-};
-fs.appendFile('greet-user.txt', `Hello ${user.username} \n `, onError);
+switch (command) {
+  case 'add':
+    notes.addNote(argv.title, argv.content);
+    break;
+  case 'remove':
+    notes.removeNote(argv.title);
+    break;
+  case 'list':
+    notes.getAllNotes();
+    break;
+  case 'read':
+    notes.getNote(argv.title);
+    break;
+  default:
+    console.log('Sorry we are not able to understand your command!', );
+    break;
+}
+console.log('args', process.argv); // argument vector
--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
-addNote = (content, file) => {
-  console.log('add note');
+const onError = (err) => {
+  if (!err) return;
+  console.log('Sorry, we were not able to create that note! See why -->', err);
+};
+
+addNote = (title, content) => {
+  console.log('add note', title, content);
+};
+
+removeNote = (title) => {
+  console.log('add note', title);
+};
+
+getAllNotes = () => {
+  console.log('Get all notes');
+};
+
+getNote = (title) => {
+  console.log('get note', title);
 };
 
 module.exports = {
   addNote,
+  getAllNotes,
+  getNote,
+  removeNote,
 };
